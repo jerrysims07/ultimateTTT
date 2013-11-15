0af053eb5b4396ce89abb9ef992122cc31fe7a52
Scott <sscotth@gmail.com>
2013-11-15 11:25:55 -0600

    default board array

--- models/game.js
+++ models/game.js
@@ -2,6 +2,17 @@ var mongoose = require('mongoose');
 
 var Game = mongoose.Schema({
   players   : [{type: mongoose.Schema.Types.ObjectId, ref: 'Player'}],
+  board     : {type: Array, default:
+                [0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0]
+              };
   createdAt : {type: Date, default: Date.now}
 });
 
--- sockets/common.js
+++ sockets/common.js
@@ -1,6 +1,13 @@
+var async = require('async')
+var __ = require('lodash')
+var io
+
 exports.connection = function(socket){
-  socket.emit('connected', {status: 'connected'});
-  socket.on('disconnect', socketDisconnect);
+  io = this
+  socket.emit('connected', {status: 'connected'})
+  socket.on('disconnect', socketDisconnect)
+  socket.on('startgame', socketStartGame)
+  socket.on('move', socketMove)
 };
 
 function socketDisconnect(){
